490abbe00ef5a6b7adb079666579aba8cb9c0404
Ziah <zvanblerck1@gmail.com>
2026-03-17 04:49:01 +0200

    update for now

--- Scenes/library_main.gd
+++ Scenes/library_main.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+
+signal iLoveYussie 
 @onready var receptionist_label_3d: Label3D = $NavigationRegion3D/Map/Misc/Desk/ReceptionistLabel3D
 @onready var door_anim_player: AnimationPlayer = $Day1/Door/door/AnimationPlayer
 @onready var bell_sound: AudioStreamPlayer3D = $Scare_1/Cutscene2/BellSound
