55cc3b42e52040b18b0d9b7c5d7b833d0c501c37
Ziah <zvanblerck1@gmail.com>
2026-03-30 19:16:22 +0200

    life is good

--- Assets/credits.gd
+++ Assets/credits.gd
@@ -3,5 +3,6 @@ extends Node2D
 var next_scene = preload("res://Scenes/ScreenMenu/StartScreenMenu.tscn")
 
 func _on_video_stream_player_finished() -> void:
-	get_tree().quit()
+	#get_tree().quit()
+	await get_tree().create_timer(4.0).timeout
 	get_tree().change_scene_to_packed(next_scene)
